d0c3cc1dfc554b4b2c1f6b3b90775e10bcaf933a
AtharvRedij <atharvaredij@gmail.com>
2020-06-23 15:42:17 +0530

    published v0.1.0
    added different types of logs like info, warn, danger, etc. with colors

---
+++ README.md
@@ -0,0 +1,19 @@
+# logs for life
+
+logs for life is a simple javascript logging library.
+
+### Installation
+
+`npm i logs-for-life`
+
+### Usage
+
+```javascript
+import lfl from "logs-for-life";
+
+lfl.log("Executed this function");
+lfl.success("Account created successfully");
+lfl.info("Server started");
+lfl.warn("Memory usage is over 90%");
+lfl.error("Network disconnected");
+```
--- index.js
+++ index.js
@@ -1,10 +1,47 @@
-// This is v0.0.1
+// This is v0.1.0
 // simple console log
 
-const log = (message) => {
-  console.log(message);
+const DEFAULT_FONT_COLOR = "#0275d8";
+const SUCCESS_FONT_COLOR = "#5cb85c";
+const INFO_FONT_COLOR = "#5bc0de";
+const WARN_FONT_COLOR = "#f0ad4e";
+const ERROR_FONT_COLOR = "#d9534f";
+
+const DEFAULT_FONT_WEIGHT = "400";
+const DEFAULT_FONT_SIZE = "1.3em";
+
+const log = (
+  message,
+  fontColor = DEFAULT_FONT_COLOR,
+  fontWeight = DEFAULT_FONT_WEIGHT,
+  fontSize = DEFAULT_FONT_SIZE
+) => {
+  console.log(
+    "%c" + message,
+    `color: ${fontColor}; font-weight: ${fontWeight}; font-size: ${fontSize}`
+  );
+};
+
+const success = (message) => {
+  log(message, SUCCESS_FONT_COLOR);
+};
+
+const info = (message) => {
+  log(message, INFO_FONT_COLOR);
+};
+
+const warn = (message) => {
+  log(message, WARN_FONT_COLOR);
+};
+
+const error = (message) => {
+  log(message, ERROR_FONT_COLOR);
 };
 
 module.exports = {
   log,
+  success,
+  info,
+  warn,
+  error,
 };
